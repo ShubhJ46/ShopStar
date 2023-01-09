f5c4204307c1fe768a2f204e7bfc021dea8fc406
Shubham <jainshubham37@gmail.com>
2023-01-09 20:27:41 +0530

    deployment

---
+++ frontend/build/precache-manifest.a77e225546319618ac4da26d17123538.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b8dadbc2f36edcc523d35d58f570f4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2aafed1fe04ae00c02d",
+    "url": "/static/css/main.9a9a9c14.chunk.css"
+  },
+  {
+    "revision": "9b7670ac5d93113c567f",
+    "url": "/static/js/2.8d78a7b8.chunk.js"
+  },
+  {
+    "revision": "4cf5c6c6f464cc15458a86cbdc669a15",
+    "url": "/static/js/2.8d78a7b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2aafed1fe04ae00c02d",
+    "url": "/static/js/main.b047738c.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
